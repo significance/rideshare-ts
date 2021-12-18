6058fd890d46675a08e35d2861ef768ebd6eae94
sig <dan@1up.digital>
2021-12-18 14:07:34 +0000

    example crapp with web3-react

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useWeb3React } from "@web3-react/core";
 
-function App() {
+import React from "react";
+
+import "./App.css";
+import { InjectedConnector } from "@web3-react/injected-connector";
+
+import { useWeb3React } from "@web3-react/core";
+
+const injectedConnector = new InjectedConnector({
+  supportedChainIds: [100],
+});
+
+const App = () => {
+  const context = useWeb3React();
+  const web3React = useWeb3React();
+
+  const { activate } = context;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <button
+      onClick={() => {
+        activate(injectedConnector);
+      }}
+    >
+      Click to activate
+    </button>
   );
-}
+};
 
 export default App;
